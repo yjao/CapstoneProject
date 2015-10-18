04bb3d2f40c6ee4aaec465a21424d1a2d2897f15
Shingo379 <pto1@uci.edu>
2015-10-17 17:20:59 -0700

    Adding Dialog Test

---
+++ KarmaTrauma/New Unity Project 1/Assets/Dialog_Test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    public GameObject Dialog;
+    GameObject t;
+	// Use this for initialization
+	void Start () {
+	    //t = (Instantiate(Dialog, new Vector3(0,0,0), Quaternion.identity)) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q) && t != null)
+            t.GetComponent<Textbox>().DrawBox("Alfred", "Peanut Butter");
+        if (Input.GetKeyDown(KeyCode.W))
+            GameObject.Destroy(t);
+        if (Input.GetKeyDown(KeyCode.E) && t == null)
+            t = (Instantiate(Dialog, new Vector3(0, 0, 0), Quaternion.identity)) as GameObject;
+	}
+}

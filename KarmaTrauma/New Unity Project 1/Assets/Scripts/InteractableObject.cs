26a1242fa27b059d7498f8a0cb42d415e60b97a8
My Hoa Tran <tranmh1@uci.edu>
2015-10-19 19:35:16 -0700

    Object Interaction

--- KarmaTrauma/New Unity Project 1/Assets/Scripts/InteractableObject.cs
+++ KarmaTrauma/New Unity Project 1/Assets/Scripts/InteractableObject.cs
@@ -3,12 +3,22 @@ using System.Collections;
 
 public class InteractableObject : MonoBehaviour
 {
+    private bool collided = false;
 	void OnTriggerEnter2D(Collider2D c)
 	{
 		if (c.gameObject.tag == "Player")
 		{
-			Debug.Log("Collided");
+			collided = true;
 		}
-	}
 
+	}
+    void Update()
+    {
+        if (collided && Input.GetKey(KeyCode.Space))
+        {
+            //call dialogue box;
+            Debug.Log("Interacting");
+            collided = false;
+        }
+    }
 }

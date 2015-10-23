a122b0b4a1eb52c65ae0db4d674f1175bb94be0e
Faye Jao <yjao@uci.edu>
2015-10-22 17:24:17 -0700

    Working on Prologue

--- KarmaTrauma/New Unity Project 1/Assets/Scripts/PrologueManager.cs
+++ KarmaTrauma/New Unity Project 1/Assets/Scripts/PrologueManager.cs
@@ -3,5 +3,36 @@ using System.Collections;
 
 public class PrologueManager : MonoBehaviour
 {
+	private GameManager gameManager;
+	private const string SCENE_PROLOGUE = "Prologue";
+	private const string SCENE_HOUSE = "House in Development";
+	public int StoryProgress;
 
+	void Start()
+	{
+		gameManager = GameManager.Instance;
+		DontDestroyOnLoad(this);
+	}
+
+	bool Progress(int value)
+	{
+		return StoryProgress == value;
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			switch (Application.loadedLevelName)
+			{
+			case SCENE_PROLOGUE:
+				if (Progress(0))
+					gameManager.CreateDialogue("", "YOU TWISTED FATE!");
+				else if (Progress(1))
+					Application.LoadLevel(SCENE_HOUSE);
+				break;
+			}
+			StoryProgress++;
+		}
+	}
 }

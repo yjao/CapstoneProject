1f812b59892a96f0cf21cb3ec096c15edcc58a81
My Hoa Tran <tranmh1@uci.edu>
2015-11-02 19:20:55 -0800

    Collision

---
+++ KarmaTrauma/New Unity Project 1/Assets/Scripts/InvisibleWallLeft.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvisibleWallLeft : MonoBehaviour {
+
+    Player player;
+    private bool colliding = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType(typeof(Player)) as Player;
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            //Debug.Log("hitting");
+            colliding = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            //Debug.Log("gettingout");
+            colliding = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float wall_x = this.transform.position.x;
+        float scale_x = this.transform.localScale.x;
+        //Debug.Log(wall_x);
+        wall_x = wall_x + scale_x / 2;
+        if (colliding)
+        {
+            player.onHitLeft(wall_x);
+
+        }
+        //Debug.Log(colliding);
+
+    }
+}
+
--- KarmaTrauma/New Unity Project 1/Assets/Scripts/Player.cs
+++ KarmaTrauma/New Unity Project 1/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ using System.IO;
 
 public class Player : MonoBehaviour
 {
+    public static InvisibleWallTop walltop;
 	private GameManager gameManager;
     public float speed;
     Animator animator;
@@ -33,8 +34,123 @@ public class Player : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        walltop = FindObjectOfType(typeof(InvisibleWallTop)) as InvisibleWallTop;
 		gameManager = GameManager.Instance;
         animator = GetComponent<Animator>();
+    }
+
+    public void onHitTop(float wall_y)
+    {
+        //Debug.Log("hittingTop");
+        float charposy = this.transform.position.y;
+        float charposx = this.transform.position.x;
+        //Debug.Log("char " + charposy + "wally: " + wall_y);
+        if (charposy >= wall_y)
+        {
+            
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                
+                //transform.position = new Vector3(Mathf.Clamp;
+                this.transform.position = new Vector3(charposx, wall_y);
+                //transform.Translate(0, 0, 0);
+                animator.SetInteger(animationState, up);
+                
+            }
+
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(0, -speed, 0);
+                animator.SetInteger(animationState, down);
+                
+            }
+        }
+    }
+
+    public void onHitBot(float wall_y)
+    {
+        //Debug.Log("hittingTop");
+        float charposy = this.transform.position.y;
+        float charposx = this.transform.position.x;
+        //Debug.Log("char " + charposy + "wally: " + wall_y);
+        if (charposy <= wall_y)
+        {
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+
+                //transform.position = new Vector3(Mathf.Clamp;
+                this.transform.position = new Vector3(charposx, wall_y);
+                //transform.Translate(0, 0, 0);
+                animator.SetInteger(animationState, down);
+
+            }
+
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(0, speed, 0);
+                animator.SetInteger(animationState, up);
+
+            }
+        }
+    }
+
+    public void onHitRight(float wall_x)
+    {
+        //Debug.Log("hittingTop");
+        float charposy = this.transform.position.y;
+        float charposx = this.transform.position.x;
+        Debug.Log("charx " + charposx + " chary " + charposy + "wallx: " + wall_x); 
+        if (charposx >= wall_x)
+        {
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                //Debug.Log("charx " + charposx + " chary " + charposy + "wallx: " + wall_x);
+                //transform.position = new Vector3(Mathf.Clamp;
+                this.transform.position = new Vector3(wall_x, charposy);
+                //transform.Translate(0, 0, 0);
+                animator.SetInteger(animationState, right);
+
+            }
+
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Debug.Log("here");
+                transform.Translate(-speed ,0, 0);
+                animator.SetInteger(animationState, left);
+
+            }
+        }
+    }
+
+    public void onHitLeft(float wall_x)
+    {
+        //Debug.Log("hittingTop");
+        float charposy = this.transform.position.y;
+        float charposx = this.transform.position.x;
+        //Debug.Log("charx " + charposx + " chary " + charposy + "wallx: " + wall_x); 
+        if (charposx <= wall_x)
+        {
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                //Debug.Log("charx " + charposx + " chary " + charposy + "wallx: " + wall_x);
+                //transform.position = new Vector3(Mathf.Clamp;
+                this.transform.position = new Vector3(wall_x, charposy);
+                //transform.Translate(0, 0, 0);
+                animator.SetInteger(animationState, left);
+
+            }
+
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Debug.Log("here");
+                transform.Translate(speed, 0, 0);
+                animator.SetInteger(animationState, left);
+
+            }
+        }
     }
 
     // Update is called once per frame

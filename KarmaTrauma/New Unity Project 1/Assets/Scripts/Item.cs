b32a93cde1cccee73e6554b7849e8333f78a2c7a
Shingo379 <pto1@uci.edu>
2015-11-05 15:20:49 -0800

    Menu and Inventory Menu

---
+++ KarmaTrauma/New Unity Project 1/Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item{
+    public string Name;
+    public string Description;
+    public string Filename;
+
+    public Item(string name, string description, string filename)
+    {
+        Name = name;
+        Description = description;
+        Filename = filename;
+    }
+}

6d9209ff3233885651444ba1d61db501ac8db411
Faye Jao <yjao@uci.edu>
2015-11-05 15:47:05 -0800

    added StrangerName to character class

--- KarmaTrauma/New Unity Project 1/Assets/Scripts/Interactable.cs
+++ KarmaTrauma/New Unity Project 1/Assets/Scripts/Interactable.cs
@@ -4,16 +4,19 @@ using System.Collections;
 public class Interactable
 {
 	public string Name;
+	public string StrangerName = "???";
 	public enum OBJECT_TYPE { NPC, OBJECT };
 	public OBJECT_TYPE Type;
 	public int ID;
 	public string[] Dialogue;
 	public int LastDialogueDisplayed;
 
-	public Interactable(string name, string[] dialogue)
+	public Interactable(string name, string strangerName, string[] dialogue)
 	{
 		LastDialogueDisplayed = -1;
 		Name = name;
+		if (strangerName.Length > 0)
+			StrangerName = strangerName;
 		Dialogue = dialogue;
 	}
 }

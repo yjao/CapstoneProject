7011b0b4200c080f2de4ddafd74861c3aea56ef8
Faye Jao <msfyjao@gmail.com>
2015-11-21 22:48:44 -0800

    minor changes to EventManager structure and MrLy scene.

--- KarmaTrauma/New Unity Project 1/Assets/Scripts/DataLoader.cs
+++ KarmaTrauma/New Unity Project 1/Assets/Scripts/DataLoader.cs
@@ -6,23 +6,24 @@ public class DataLoader
 	public DataLoader()
 	{
 		Debug.Log("Loading Game Data");
-
+		
 		Interactable intr = new Interactable();
 		intr.ID = 65;
 		intr.Name = "Mr. Ly";
 		intr.StrangerName = "???";
 		intr.objectType = Interactable.ObjectType.NPC;
-		string[] testDialog = new string[] {"You should be in class, not here playing with jewels."};
+		string[] testDialog = new string[] {"Oh. Um, I didn't see you there. You should be in class, not here playing with jewels.",
+			"I'm telling you, go back to class!"};
 		intr.Dialogue = new Dialogue[10];
 		for (int i = 0; i < testDialog.GetLength(0); i++)
 		{
 			Dialogue dialogue = new Dialogue(i, testDialog[i]);
 			//dialogue.text = testDialog[i];
-
+			
 			intr.Dialogue[i] = dialogue;
 		}
 		GameManager.Instance.AllObjects[intr.ID] = intr;
-
+		
 		Debug.Log("Loading Complete");
 	}
 }

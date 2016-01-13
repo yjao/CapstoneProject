bc56db8ada9c9590d0814c355c8840f4c2e03eed
My Hoa Tran <tranmh1@uci.edu>
2016-01-12 18:01:16 -0800

    Still making changes to menu and gamemanager.  Use the new gamemanager so your game doesn't crash (hopefully).  Remove the Menu and Questlog from Player prefab.

--- KarmaTrauma/New Unity Project 1/Assets/Scripts/Menu_Layout.cs
+++ KarmaTrauma/New Unity Project 1/Assets/Scripts/Menu_Layout.cs
@@ -4,7 +4,8 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-public class Menu_Layout : MonoBehaviour {
+public class Menu_Layout : MonoBehaviour
+{
 
     GameManager gameManager;
     QuestManager questManager;
@@ -19,11 +20,12 @@ public class Menu_Layout : MonoBehaviour {
     private int world_map_level = 17;
     private Texture2D inventory_texture;
     private Texture2D diary_texture;
-   // public GUIContent inventory_content;
-   
-	// Use this for initialization
-	void Start () {
-        
+    // public GUIContent inventory_content;
+
+    // Use this for initialization
+    void Start()
+    {
+
         gameManager = GameManager.instance;
         questManager = QuestManager.Instance;
         //gameMenu = FindObjectOfType(typeof(Menu)) as Menu;
@@ -39,22 +41,22 @@ public class Menu_Layout : MonoBehaviour {
 
         transform.FindChild("QuestBook").gameObject.SetActive(false);
         transform.FindChild("Inventory").gameObject.SetActive(false);
-       
-	}
+
+    }
 
     //void OnGUI()
     //{
     //    GUI.backgroundColor = Color.clear;
-        
+
     //    string time = gameManager.GetTime();
-        
+
     //    bool open_menu = GameObject.FindGameObjectWithTag("Menu");
     //    //GUI.skin.button.normal.background = inventory_texture;
     //    //GUI.skin.toggle.hover.background = inventory_texture;
     //    //GUI.skin.toggle.active.background = inventory_texture;
-        
+
     //    timeButton_show = GUI.Button(new Rect(0,0,100,40), time);
-        
+
     //    invenButton_show = GUI.Button(new Rect(0, 40, 40, 40), inventory_texture);
 
     //    if (invenButton_show && !open_menu)
@@ -66,7 +68,7 @@ public class Menu_Layout : MonoBehaviour {
     //    diaryButton_show = GUI.Button(new Rect(0, 80, 40, 40), "Diary");
 
     //    //Find a place to put this:  Dataloader
-       
+
 
     //    if (diaryButton_show && !open_menu)
     //    {
@@ -76,8 +78,8 @@ public class Menu_Layout : MonoBehaviour {
 
     //    }
 
-        
-        
+
+
     //}
     void M_Clock()
     {
@@ -100,9 +102,19 @@ public class Menu_Layout : MonoBehaviour {
         transform.FindChild("Inventory").gameObject.SetActive(true);
     }
 
+    private void B_Bag()
+    {
+        transform.FindChild("Inventory").gameObject.SetActive(true);
+        invenButton_show = true;
+        gameManager.gameMode = GameManager.GameMode.MENU;
+        gameManager.prevMode = GameManager.GameMode.PLAYING;
+
+    }
+
     public void Close_Bag()
     {
         transform.FindChild("Inventory").gameObject.SetActive(false);
+        //invenButton_show = false;
         gameManager.Play();
     }
 
@@ -130,9 +142,18 @@ public class Menu_Layout : MonoBehaviour {
             transform.FindChild("Fast_forward").gameObject.SetActive(false);
         }
     }
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update() {
         M_Clock();
-	
+	    if (Input.GetKeyDown(KeyCode.B)){
+            B_Bag();
+        }
+        //if (invenButton_show){
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        Close_Bag();
+               
+        //    }
+        //}
 	}
 }

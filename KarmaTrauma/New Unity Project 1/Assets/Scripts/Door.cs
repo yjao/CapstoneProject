95a559d39ef8fc6f090dda0f29d0d1f784170e87
Jeney Lao <jeneyl@uci.edu>
2016-02-09 16:03:09 -0800

    All doors and teleporters are now interactable by spacebar.

--- KarmaTrauma/New Unity Project 1/Assets/Scripts/Door.cs
+++ KarmaTrauma/New Unity Project 1/Assets/Scripts/Door.cs
@@ -9,36 +9,58 @@ public class Door : MonoBehaviour
 	public bool increaseTime = true;
     //public PlayerData playerData;
     //public DayData dayData = new DayData();
+    private bool colliding = false;
+    private string destination;
 
     void Start()
     {
         gameManager = GameManager.instance;
         //playerData = PlayerData.Instance;
     }
+
+    void Update()
+    {
+        if (colliding == true && Input.GetKey(KeyCode.Space))
+        {
+            if (increaseTime)
+            {
+                gameManager.IncreaseTime();
+                //Debug.Log("gameClock : " + gameManager.GetTimeAsInt());
+            }
+            bool midnight = gameManager.Midnight();
+            destination = "WorldMap";
+            if (AltDestination != "")
+            {
+                destination = AltDestination;
+            }
+
+            if (midnight)
+            {
+                destination = "G_House";
+            }
+            
+        SceneManager.instance.LoadScene(destination);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player")
         {
-			if (increaseTime)
-			{
-	            gameManager.IncreaseTime();
-                //Debug.Log("gameClock : " + gameManager.GetTimeAsInt());
-			}
-			bool midnight = gameManager.Midnight();
-			string destination = "WorldMap";
-            if (AltDestination != "")
-            {
-				destination = AltDestination;
-            }
-
-			if (midnight)
-			{
-				destination = "G_House";
-			}
-
-			SceneManager.instance.LoadScene(destination);
+            colliding = true;
         }
     }
+
+    void OnTriggerExit2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Player")
+            colliding = false;
+
+        /*if (player.CollidingWithID.Contains(ID))
+            player.CollidingWithID.Remove(ID);*/
+    }
+
+
     void ChangeDestination(string newDestination)
     {
         AltDestination = newDestination;

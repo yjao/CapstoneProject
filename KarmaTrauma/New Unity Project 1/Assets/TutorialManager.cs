a5e49453f66c57cd52e81cc76c5d77aa0eb6c646
Faye Jao <yjao@uci.edu>
2016-02-22 22:40:00 -0800

    setting up WP2 and tutorial manager

---
+++ KarmaTrauma/New Unity Project 1/Assets/TutorialManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+
+	IEnumerator Slide1_Coroutine()
+	{
+		float countdown = 6;
+
+
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}

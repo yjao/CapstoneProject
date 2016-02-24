f7ccf39a576cd0607b25fed98c2388a04bd80072
Jeney Lao <jeneyl@uci.edu>
2016-02-23 23:32:31 -0800

    SLIDE 4 IS LIKE, DONE (if you don't know what this means, look at #4 on the doc for tutorial)

--- KarmaTrauma/New Unity Project 1/Assets/TutorialManager.cs
+++ KarmaTrauma/New Unity Project 1/Assets/TutorialManager.cs
@@ -13,6 +13,8 @@ public class TutorialManager : MonoBehaviour
 
     private const string SCENE_MINI_MAIN_STREET = "T_MiniMainStreet";
 
+    private GameObject activeTutorialBox;
+
     public GameObject dialogueContainer;
     private List<Slide> slides;
 
@@ -60,7 +62,7 @@ public class TutorialManager : MonoBehaviour
         if (slide.text != null)
         {
             yield return new WaitForSeconds(1f);
-            CreateTutorialBox(slide.text, slide.timer - 1f, Textbox.TutorialBoxPosition.BOTTOM);
+            CreateTutorialBox(slide.text, Textbox.TutorialBoxPosition.BOTTOM, slide.timer - 1f);
             yield return null;
         }
         yield return new WaitForSeconds(slide.timer);
@@ -74,7 +76,25 @@ public class TutorialManager : MonoBehaviour
         yield return StartCoroutine(GameObject.Find("Invis").GetComponent<CharacterAnimations>().Move(1, -12.5f, CharacterAnimations.States.DOWN_WALK));
 
         GameObject.Find("Invis").transform.parent = GameObject.Find("Player").transform;
+        CreateTutorialBox("What’s wrong? Have you forgotten how to walk? Haha, you’re so awkward Chels! It’s why I love ya. #Use the arrow keys or WASD keys to move# your butt. Now go #get the ball#!", Textbox.TutorialBoxPosition.BOTTOM);
         gameManager.Play();
+        yield break;
+    }
+
+    public IEnumerator Slide4_Triggers_Coroutine(string tag)
+    {
+        if (tag == "TrafficLight")
+        {
+            CreateTutorialBox("Um, yeah, you might want to hit the pedestrian light before crossing. What? Hey, are you spacing out again? #Space bar#, not space out! Press that and like, #interact with objects#.", Textbox.TutorialBoxPosition.BOTTOM);
+        }
+        else if (tag == "Kelly")
+        {
+            CreateTutorialBox("What is it? Come on, get your nose out of your book and go exercise a bit! Go, #get the ball#! For me!", Textbox.TutorialBoxPosition.BOTTOM);
+        }
+        else if (tag == "traffic")
+        {
+            CreateTutorialBox("Hrm… I guess it doesn’t work. Well it’s whatever, I don’t see any cops; just be careful! If I were you, I’d #move while holding shift key to run#. The cars are less likely to hit you if you run faster, right?", Textbox.TutorialBoxPosition.BOTTOM);
+        }
         yield break;
     }
 
@@ -85,11 +105,15 @@ public class TutorialManager : MonoBehaviour
     }
 
 
-    public GameObject CreateTutorialBox(string message, float destroyTimer = -1, Textbox.TutorialBoxPosition position = Textbox.TutorialBoxPosition.MIDDLE)
+    public void CreateTutorialBox(string message, Textbox.TutorialBoxPosition position = Textbox.TutorialBoxPosition.MIDDLE, float destroyTimer = -1)
     {
+        if (activeTutorialBox != null)
+        {
+            Destroy(activeTutorialBox);
+        }
         GameObject dialog = (GameObject)Instantiate(dialogueContainer, dialogueContainer.transform.position, Quaternion.identity);
         StartCoroutine(dialog.GetComponent<Textbox>().DrawTutorialBox(message, destroyTimer, position));
-        return dialog;
+        activeTutorialBox = dialog;
     }
 
 	// Update is called once per frame

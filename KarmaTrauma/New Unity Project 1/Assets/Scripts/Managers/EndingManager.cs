6bc8c697eb0ebc89937bd493f76fd35ef562805c
Faye Jao <yjao@uci.edu>
2016-03-15 00:57:47 -0700

    DaeEnding is finished

--- KarmaTrauma/New Unity Project 1/Assets/Scripts/Managers/EndingManager.cs
+++ KarmaTrauma/New Unity Project 1/Assets/Scripts/Managers/EndingManager.cs
@@ -40,14 +40,75 @@ public class EndingManager : MonoBehaviour {
         EndingManager.instance.StartCoroutine(args.CoroutineName);
     }
 
-    public IEnumerator DaeEnding()
-    {
+	public IEnumerator DaeEnding()
+	{
+		yield return StartCoroutine(sm.fade_black());
+		gm.MenuLayout.GetComponent<Menu_Layout>().GameMenus(false);
+		gm.MenuLayout.GetComponent<Menu_Layout>().timeTint.SetActive(false);
 		yield return StartCoroutine(LoadSceneCoroutine("Ending"));
-		yield return sm.fade_black();
-        yield return StartCoroutine(Slide_Coroutine(new TutorialManager.Slide("Slide33(2)", 20, "Jerry Faraday: Thank you, thank you everybody! I'll make our county a much, much better place, turning this land into a wealthy land of gold!")));
-		yield return null;
+		yield return new WaitForSeconds(1f);
+		CreateDialogue("Dae", "\"Here. This is what you wanted. Now where is the heart you promised?\"");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Patricia", "\"Very well. First thing after the polls come out.\"");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Patricia", "\"Don't worry, it's a good deed you've done for your daughter.\"");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Patricia", "\"I would have done the exact same thing for mine.\"");
+		yield return null; while (Pause()) { yield return null; }
+		StartCoroutine(sm.fade_out());
+		yield return StartCoroutine(Slide_Coroutine(new TutorialManager.Slide("Slide33(2)", 8, "Jerry Faraday: Thank you, thank you everybody! I'll make our county a much, much better place, turning this land into a wealthy land of gold!"), false));
+		yield return StartCoroutine(sm.fade_black());
+        yield return new WaitForSeconds(1f);
+		yield return StartCoroutine(Slide_Coroutine(new TutorialManager.Slide("Slide7", 5, "Chelsey: And so, it turns out that Jerry Faraday had won the election."), false));
+		StartCoroutine(LoadSceneCoroutine("E_Mall"));
+		yield return StartCoroutine(sm.fade_out());
+		CreateDialogue("Jeney", "\"So, it still happened.\"");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Faye", "(Was it because I didn't vote?)");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Jeney", "\"Well, it's been nice having you around. You've been a great help.\"");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Faye", "(Help with eating all your Potadonut Tots...)");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Faye", "\"I mean, my pleasure working here. I'm still sorry about the shop.\"");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Jeney", "\"Don't worry about it. I'm sure I'll find somewhere great.\"");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Jeney", "\"I'll definitely miss Punxsutown, though.\"");
+		yield return null; while (Pause()) { yield return null; }
+        yield return StartCoroutine(sm.fade_black());
+		yield return StartCoroutine(LoadSceneCoroutine("E_Park"));
+		yield return StartCoroutine(sm.fade_out());
+		CreateDialogue("Yoona", "\"Alex, I just want to say...\"");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Alex", "\"You don't have to say anything. Just enjoy the new world!\"");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Hank", "\"Hey, kids! I said, be quiet!\"");
+		yield return null; while (Pause()) { yield return null; }
+		yield return new WaitForSeconds(1f);
+		CreateDialogue("Yoona", "\"Thank you, Alex... *blush*\"");
+		yield return null; while (Pause()) { yield return null; }
+		CreateDialogue("Yoona", "\"And thank you, Daddy.\"");
+		yield return null; while (Pause()) { yield return null; }
+		yield return StartCoroutine(sm.fade_black());
+		yield return StartCoroutine(LoadSceneCoroutine("Ending"));
+		StartCoroutine(sm.fade_out());
+		yield return StartCoroutine(Slide_Coroutine(new TutorialManager.Slide("Slide7", 4, "Chelsey: All this time loop, was to save Alfred."), false));
+		yield return StartCoroutine(Slide_Coroutine(new TutorialManager.Slide("Slide7", 4.5f, "Chelsey: Even though Faraday was elected as senator, nobody was hurt."), false));
+		yield return StartCoroutine(Slide_Coroutine(new TutorialManager.Slide("Slide7", 5, "Chelsey: Of course, some people still had to sacrifice, just for a bit of peace."), false));
+		yield return StartCoroutine(sm.fade_black());
+		yield return new WaitForSeconds(1f);
+		CreateTutorialBox(" ~ The End ~ ", standalone: true);
+		yield return new WaitForSeconds(5f);
+        CreateDialogue("Mr. Ly", "\"YES! OH YES, FARADAY! Mah boy! Woohoo!!\"");
+        yield return null; while (Pause()) { yield return null; }
+        while (true)
+        {
+            yield return null;
+        }
+        yield return null;
     }
-
+    
     public IEnumerator AlfredEnding()
     {
 		yield return StartCoroutine(sm.fade_black());
@@ -107,7 +168,7 @@ public class EndingManager : MonoBehaviour {
 		yield return new WaitForSeconds(1f);
 		CreateTutorialBox(" ~ The End ~ ", standalone: true);
 		yield return new WaitForSeconds(5f);
-		CreateDialogue("Mr. Ly", "\"NO! MAH BOI, FARADAY! Why aren't you elected...!??!??\"");
+		CreateDialogue("Mr. Ly", "\"NO! MAH BOY, FARADAY! Why aren't you elected...!??!??\"");
 		yield return null; while (Pause()) { yield return null; }
 		while (true)
 		{
